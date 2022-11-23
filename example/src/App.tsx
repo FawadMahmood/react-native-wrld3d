b161b2ea3b2aa8313a7189b16ecf902c711d2c1f
FawadMahmood <fawad.mahmood@outlook.com>
2022-11-23 03:39:08 -0800

    fix: crash issue
    fixed crashed issue when recreating map instance

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-import { Alert, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { Wrld3dView, Marker, MapViewRefPropsType } from 'react-native-wrld3d';
 // , 1, 2
 export default function App() {
@@ -8,92 +15,92 @@ export default function App() {
   const [ready, setReady] = React.useState(false);
   const [cache, setCache] = React.useState(false);
 
-
   const ref = React.useRef<MapViewRefPropsType>();
-
 
   let markers = [];
   for (let i = 0; i < 100; i++) {
-
-    markers.push(
-      {
-        location: {
-          latitude: 37.802355,
-          longitude: -122.405848
-        },
-        image: "https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true"
+    markers.push({
+      location: {
+        latitude: 37.802355,
+        longitude: -122.405848,
       },
-    )
+      image:
+        'https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true',
+    });
   }
-
 
   const [markets, setMarkers] = React.useState([
     ...markers,
     {
       location: {
         latitude: 37.802355,
-        longitude: -122.405848
+        longitude: -122.405848,
       },
-      image: "https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true"
+      image:
+        'https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true',
     },
     {
       location: {
-        latitude: 37.802380,
-        longitude: -122.405848
+        latitude: 37.80238,
+        longitude: -122.405848,
       },
-      image: "https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true"
+      image:
+        'https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true',
     },
     {
       location: {
         latitude: 37.802395,
-        longitude: -122.405848
+        longitude: -122.405848,
       },
-      image: "https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true"
-    }
+      image:
+        'https://github.com/stasgora/round-spot/blob/master/assets/logo.png?raw=true',
+    },
   ]);
 
-
-
   const onAddMarker = () => {
-
     setMarkers([
       {
         location: {
           latitude: 37.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        image:
+          'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
       },
       {
         location: {
           latitude: 39.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://images.unsplash.com/photo-1485206412256-701ccc5b93ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        image:
+          'https://images.unsplash.com/photo-1485206412256-701ccc5b93ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
       },
       {
         location: {
           latitude: 40.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF_2O5ngxoY3B4Vb-d6MRsS_-F0moFSI39SQ&usqp=CAU"
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTF_2O5ngxoY3B4Vb-d6MRsS_-F0moFSI39SQ&usqp=CAU',
       },
       {
         location: {
           latitude: 35.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYCNlYpuNV9PfNMzlNT9SAHTvkPLjI_OmRw&usqp=CAU"
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYCNlYpuNV9PfNMzlNT9SAHTvkPLjI_OmRw&usqp=CAU',
       },
       {
         location: {
           latitude: 42.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS9uSS5KRJCiR_cXunDz3hKtCLBzFsQAs3__yg0hkpt7kt4AJAxq1SwGg0250nGqDOLSc&usqp=CAU"
-      }
-    ])
-  }
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS9uSS5KRJCiR_cXunDz3hKtCLBzFsQAs3__yg0hkpt7kt4AJAxq1SwGg0250nGqDOLSc&usqp=CAU',
+      },
+    ]);
+  };
 
   const removeMarker = () => {
     // setMarkers([]);
@@ -101,49 +108,51 @@ export default function App() {
       {
         location: {
           latitude: 65.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS9uSS5KRJCiR_cXunDz3hKtCLBzFsQAs3__yg0hkpt7kt4AJAxq1SwGg0250nGqDOLSc&usqp=CAU"
-
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS9uSS5KRJCiR_cXunDz3hKtCLBzFsQAs3__yg0hkpt7kt4AJAxq1SwGg0250nGqDOLSc&usqp=CAU',
       },
       {
         location: {
           latitude: 62.802355,
-          longitude: -122.405848
+          longitude: -122.405848,
         },
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYCNlYpuNV9PfNMzlNT9SAHTvkPLjI_OmRw&usqp=CAU"
-      }
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYCNlYpuNV9PfNMzlNT9SAHTvkPLjI_OmRw&usqp=CAU',
+      },
     ]);
-  }
+  };
 
   const onMapReady = () => {
     setReady(true);
-  }
+  };
 
   const onMapCacheCompleted = () => {
     setCache(true);
-  }
+  };
 
   const animateToRegion = async () => {
     setVisible(false);
-  }
-
-
+  };
 
   return (
     <SafeAreaView>
       <View>
         <View style={styles.header}>
-          <Text style={styles.statusTxt}>{ready ? "Map Ready" : "Map Not Ready"}</Text>
-          <Text style={styles.statusTxt}>{cache ? "Cache Completed" : "Cache In Progress"}</Text>
+          <Text style={styles.statusTxt}>
+            {ready ? 'Map Ready' : 'Map Not Ready'}
+          </Text>
+          <Text style={styles.statusTxt}>
+            {cache ? 'Cache Completed' : 'Cache In Progress'}
+          </Text>
         </View>
-        {visible ?
+        {visible ? (
           <Wrld3dView
-
             ref={ref as { current: MapViewRefPropsType }}
             initialCenter={{
               latitude: 24.882613347789693,
-              longitude: 67.05802695237224
+              longitude: 67.05802695237224,
             }}
             onMapReady={onMapReady.bind(null)}
             onMapCacheCompleted={onMapCacheCompleted.bind(null)}
@@ -151,57 +160,98 @@ export default function App() {
             precacheDistance={5000}
             zoomLevel={15}
             key={'Wrld3dView'}
-            style={{ width: "100%", height: "80%" }}
+            style={styles.mapStyle}
           >
             {markets.map((marker, index) => {
               if (index > 3) {
                 return (
-                  <Marker elevationMode="HeightAboveGround" elevation={150} key={index} location={marker.location} style={{ width: 90, height: 90 }}>
-                    <TouchableOpacity style={{ overflow: "hidden", width: "100%", height: "100%", borderRadius: 50, justifyContent: "center", alignItems: "center" }}>
-                      <Image resizeMode='cover' source={{ uri: marker.image }} style={{ width: "100%", height: "100%" }} />
+                  <Marker
+                    elevationMode="HeightAboveGround"
+                    elevation={150}
+                    key={index}
+                    location={marker.location}
+                    style={styles.marker}
+                  >
+                    <TouchableOpacity style={styles.btnStyle}>
+                      <Image
+                        resizeMode="cover"
+                        source={{ uri: marker.image }}
+                        style={styles.flexBox}
+                      />
                     </TouchableOpacity>
                   </Marker>
-                )
+                );
               }
 
-
               return (
-                <Marker elevationMode="HeightAboveGround" elevation={150} key={index} location={marker.location} style={{ width: 90, height: 90 }}>
-                  <TouchableOpacity style={{ overflow: "hidden", width: "100%", height: "100%", borderRadius: 50, justifyContent: "center", alignItems: "center" }}>
-                    <Image resizeMode='cover' source={{ uri: marker.image }} style={{ width: "100%", height: "100%" }} />
+                <Marker
+                  elevationMode="HeightAboveGround"
+                  elevation={150}
+                  key={index}
+                  location={marker.location}
+                  style={styles.marker}
+                >
+                  <TouchableOpacity style={styles.btnStyle}>
+                    <Image
+                      resizeMode="cover"
+                      source={{ uri: marker.image }}
+                      style={styles.flexBox}
+                    />
                   </TouchableOpacity>
                 </Marker>
-              )
+              );
             })}
-          </Wrld3dView> : null}
+          </Wrld3dView>
+        ) : null}
 
-        <TouchableOpacity onPress={onAddMarker.bind(null)} style={{ width: "100%", height: 45, backgroundColor: "blue" }}>
-          <Text style={{ fontSize: 30, color: "white" }}>{"Add a marker"}</Text>
+        <TouchableOpacity
+          onPress={onAddMarker.bind(null)}
+          style={styles.btnStyle2}
+        >
+          <Text style={styles.txt}>{'Add a marker'}</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={removeMarker.bind(null)} style={{ width: "100%", height: 45, backgroundColor: "blue" }}>
-          <Text style={{ fontSize: 30, color: "white" }}>{"Add a marker"}</Text>
+        <TouchableOpacity
+          onPress={removeMarker.bind(null)}
+          style={styles.btnStyles23}
+        >
+          <Text style={styles.txt}>{'Add a marker'}</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={animateToRegion.bind(null)} style={{ width: "100%", height: 45, backgroundColor: "blue" }}>
-          <Text style={{ fontSize: 30, color: "white" }}>{"Animate to region"}</Text>
+        <TouchableOpacity
+          onPress={animateToRegion.bind(null)}
+          style={styles.btnStyles23}
+        >
+          <Text style={styles.txt}>{'Animate to region'}</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
-
 const styles = StyleSheet.create({
   header: {
     height: 40,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    paddingHorizontal: 10
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
   },
   statusTxt: {
-    fontWeight: "400"
-
-  }
+    fontWeight: '400',
+  },
+  flexBox: { width: '100%', height: '100%' },
+  btnStyle: {
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnStyle2: { width: '100%', height: 45, backgroundColor: 'blue' },
+  mapStyle: { width: '100%', height: '80%' },
+  marker: { width: 90, height: 90 },
+  txt: { fontSize: 30, color: 'white' },
+  btnStyles23: { width: '100%', height: 45, backgroundColor: 'blue' },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,14 @@ import {
   LayoutChangeEvent,
   PixelRatio,
   Platform,
+  StyleSheet,
   UIManager,
   View,
 } from 'react-native';
 import { NativeModules } from 'react-native';
 
-
 import { WrldMap3d, Marker as MarkerView, Wrld3dProps } from './MapViewManager';
 import { Commands, MapViewNativeComponentType, Region } from './type';
-
-
 
 const createFragment = (viewId: number) =>
   UIManager.dispatchViewManagerCommand(
@@ -24,46 +22,74 @@ const createFragment = (viewId: number) =>
     [viewId]
   );
 
-const processCommand = (viewId: number, command: string, args: any[]) => UIManager.dispatchViewManagerCommand(
-  viewId,
-  command,
-  args
-);
+const processCommand = (viewId: number, command: string, args: any[]) =>
+  UIManager.dispatchViewManagerCommand(viewId, command, args);
 
 export const Marker = MarkerView;
 
 export type MapViewRefPropsType = MapViewNativeComponentType;
 
-const MapComponent: React.ForwardRefRenderFunction<MapViewNativeComponentType, Wrld3dProps> = (
-  props,
-  forwardedRef,
-) => {
-
-  const viewId = useRef<number>(0)
+const MapComponent: React.ForwardRefRenderFunction<
+  MapViewNativeComponentType,
+  Wrld3dProps
+> = (props, forwardedRef) => {
+  const viewId = useRef<number>(0);
   const isMapReady = useRef<boolean>();
 
-
-  const moveToRegion = ({ location, animated = false, duration = 5000, zoomLevel = -1 }: { location: Region, animated: boolean, duration?: number, zoomLevel?: number }) => {
-    if (Platform.OS === "android") {
-      processCommand(viewId.current, Commands.animateToRegion, [location, animated, duration, zoomLevel]);
+  const moveToRegion = ({
+    location,
+    animated = false,
+    duration = 5000,
+    zoomLevel = -1,
+  }: {
+    location: Region;
+    animated: boolean;
+    duration?: number;
+    zoomLevel?: number;
+  }) => {
+    if (Platform.OS === 'android') {
+      processCommand(viewId.current, Commands.animateToRegion, [
+        location,
+        animated,
+        duration,
+        zoomLevel,
+      ]);
     }
-  }
+  };
 
-  const moveToBuilding = ({ location, highlight = false, zoomLevel = -1, animated = false, duration = 3000 }: { location: Region, highlight?: boolean, zoomLevel?: number, animated?: boolean, duration?: number }) => {
-    if (Platform.OS === "android") {
-      processCommand(viewId.current, Commands.moveToBuilding, [location, highlight, zoomLevel, animated, duration]);
+  const moveToBuilding = ({
+    location,
+    highlight = false,
+    zoomLevel = -1,
+    animated = false,
+    duration = 3000,
+  }: {
+    location: Region;
+    highlight?: boolean;
+    zoomLevel?: number;
+    animated?: boolean;
+    duration?: number;
+  }) => {
+    if (Platform.OS === 'android') {
+      processCommand(viewId.current, Commands.moveToBuilding, [
+        location,
+        highlight,
+        zoomLevel,
+        animated,
+        duration,
+      ]);
     }
-  }
-
-
+  };
 
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const ref = useRef<any>(null);
 
   useEffect(() => {
-    if (Platform.OS === "android") {
+    if (Platform.OS === 'android') {
       viewId.current = findNodeHandle(ref.current) as number;
-      createFragment(viewId.current as number);
+      setTimeout(() => {
+        createFragment(viewId.current as number);
+      }, 2000);
     }
   }, []);
 
@@ -74,27 +100,48 @@ const MapComponent: React.ForwardRefRenderFunction<MapViewNativeComponentType, W
     width: PixelRatio.getPixelSizeForLayoutSize(dimensions.width),
   };
 
-
   const onlayout = (event: LayoutChangeEvent) => {
     const { width, height } = event.nativeEvent.layout;
     setDimensions({ width: width, height: height });
-  }
-
+  };
 
   const getMapCenter = async () => {
-    const message = await NativeModules['MapViewModule'].getCameraBounds(viewId.current);
+    const message = await NativeModules['MapViewModule'].getCameraBounds(
+      viewId.current
+    );
     return message;
-  }
+  };
 
-  const getBuildingInformation = async ({ location, animateToBuilding = false, duration = 2000, zoomLevel = 18 }: { location: Region, animateToBuilding?: boolean, duration?: number, zoomLevel?: number }) => {
-    const message = await NativeModules['MapViewModule'].getBuildingInformation(viewId.current, location.longitude, location.latitude, animateToBuilding, duration, zoomLevel);
+  const getBuildingInformation = async ({
+    location,
+    animateToBuilding = false,
+    duration = 2000,
+    zoomLevel = 18,
+  }: {
+    location: Region;
+    animateToBuilding?: boolean;
+    duration?: number;
+    zoomLevel?: number;
+  }) => {
+    const message = await NativeModules['MapViewModule'].getBuildingInformation(
+      viewId.current,
+      location.longitude,
+      location.latitude,
+      animateToBuilding,
+      duration,
+      zoomLevel
+    );
     return message;
-  }
+  };
 
   const setBuildingHighlight = async ({ location }: { location: Region }) => {
-    const message = await NativeModules['MapViewModule'].addBuildingHighlight(viewId.current, location.longitude, location.latitude);
+    const message = await NativeModules['MapViewModule'].addBuildingHighlight(
+      viewId.current,
+      location.longitude,
+      location.latitude
+    );
     return message;
-  }
+  };
 
   //mapevents
   const publicRef = {
@@ -102,15 +149,20 @@ const MapComponent: React.ForwardRefRenderFunction<MapViewNativeComponentType, W
     moveToBuilding,
     getMapCenter,
     getBuildingInformation,
-    setBuildingHighlight
+    setBuildingHighlight,
   };
-
 
   useImperativeHandle(forwardedRef, () => publicRef);
 
   return (
     // <ProviderContext.Provider value={{ provider: 'wrld3dMap' }}>
-    <View onLayout={onlayout.bind(null)} style={[props.style ? props.style : { width: "100%", height: "100%" }, { overflow: "hidden" }]}>
+    <View
+      onLayout={onlayout.bind(null)}
+      style={[
+        props.style ? props.style : { width: '100%', height: '100%' },
+        styles.hiddenOver,
+      ]}
+    >
       <WrldMap3d
         {...props}
         key={new Date().toDateString()}
@@ -118,25 +170,31 @@ const MapComponent: React.ForwardRefRenderFunction<MapViewNativeComponentType, W
         // @ts-ignore
         ref={ref}
         zoomLevel={props.zoomLevel ? props.zoomLevel : 12}
-        initialCenter={props.initialCenter ? props.initialCenter : {
-          latitude: 24.8620502,
-          longitude: 67.0708794
-        }}
+        initialCenter={
+          props.initialCenter
+            ? props.initialCenter
+            : {
+                latitude: 24.8620502,
+                longitude: 67.0708794,
+              }
+        }
         onMapReady={() => {
           isMapReady.current = true;
           if (props.onMapReady) {
             props.onMapReady();
           }
         }}
-
         precache={props.precache}
         precacheDistance={props.precacheDistance}
       >
         {props.children}
       </WrldMap3d>
     </View>
-  )
-}
-
+  );
+};
 
 export const Wrld3dView = React.forwardRef(MapComponent);
+
+const styles = StyleSheet.create({
+  hiddenOver: { overflow: 'hidden' },
+});
